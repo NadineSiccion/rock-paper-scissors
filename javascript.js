8838321d5aa2175a2fa512c8d713d4736513d921
Nadine Siccion <nv.siccion@gmail.com>
2024-02-27 11:47:54 +0800

    Add functionality that checks for winner
    This functionality checks if either the computer
    or the player has reached the score required to
    win. If one has, it will display the winner and
    remove the EventListeners from the buttons so
    the game has ended.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 let getComputerChoice = function() {
     CHOICES = ['Rock', 'Paper', 'Scissors']
     randomIndex = Math.floor(Math.random() * CHOICES.length);
@@ -7,15 +8,43 @@ let getComputerChoice = function() {
 // point system
 let playerScore = 0;
 let computerScore = 0;
-
 let addPlayerScore = function() {
     playerScore += 1;
 }
-
 let addComputerScore = function() {
     computerScore += 1;
 }
 
+const announcementP = document.createElement('p');
+
+let announceWinner = function(winner) {
+    if (winner.toLowerCase() == 'player') {
+        announcementP.textContent = 'YOU are the winner!'
+        body.appendChild(announcementP);
+    } else if (winner.toLowerCase() == 'computer') {
+        announcementP.textContent = 'Computer wins!'
+        body.appendChild(announcementP);
+    }
+}
+
+let removeButtonEventListeners = function() {
+    choiceBtns.forEach((button) => {button.removeEventListener('click', buttonClick)});
+}
+
+let checkForWinner = function(max=5) {
+    if (playerScore >= max) {
+        console.log('player wins')
+        announceWinner('player');
+        removeButtonEventListeners();
+    } else if (computerScore >= max) {
+        console.log('computer wins')
+        announceWinner('computer');
+        removeButtonEventListeners();
+    }
+}
+
+
+// game logic
 let playRound = function(playerSelection, computerSelection) {
     
     playerSelection = playerSelection.toLowerCase();
@@ -56,12 +85,12 @@ let playRound = function(playerSelection, computerSelection) {
     }
 };
 
+// dynamic webpage
 const buttonDiv = document.querySelector('.buttons');
 const rockBtn = document.querySelector('#rock');
 const paperBtn = document.querySelector('#paper');
 const scissorsBtn = document.querySelector('#scissors');
 
-// const fragment = new DocumentFragment();
 const resultsDiv = document.createElement('div');
 resultsDiv.classList.add('results');
 
@@ -74,9 +103,8 @@ choicesP.setAttribute('name',"winner");
 const pointsP = document.createElement('p');
 choicesP.setAttribute('name', "points");
 
-
-const choiceBtns = document.querySelectorAll('.choice');
-choiceBtns.forEach(button => {button.addEventListener('click', (e) => {
+// button functionality
+let buttonClick = function(e) {
     playerChoice = (e.target.textContent);
     computerChoice = getComputerChoice();
     playRound(playerChoice, computerChoice)
@@ -90,9 +118,11 @@ choiceBtns.forEach(button => {button.addEventListener('click', (e) => {
     resultsDiv.appendChild(winnerP);
     resultsDiv.appendChild(pointsP);
 
-    const body = document.querySelector('body');
     body.appendChild(resultsDiv);
+    checkForWinner();
+}
 
-});
+const choiceBtns = document.querySelectorAll('.choice');
+choiceBtns.forEach(button => {button.addEventListener('click', buttonClick);
     
 });
